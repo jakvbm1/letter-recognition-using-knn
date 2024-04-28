8c66aa85adc165a0b3f8bae43bf8eea0ffdffdda
Jakub Miarka <jakvbmiarka@gmail.com>
2024-04-28 16:00:50 +0200

    Updated image->data function
    hopefully it works better than before..

--- csv_creation.py
+++ csv_creation.py
@@ -5,28 +5,34 @@ from PIL import Image
 
 def xed(np_img):
     edge_count = 0
-    for row in np_img:
+    position_count = 0
+    for row in enumerate(np_img):
         prev_pixel = 0
-        for pixel in row:
+        for index, pixel in row:
             if pixel < 255 and prev_pixel == 255:
                 edge_count += 1
+                position_count += len(np_img) - index
             prev_pixel = pixel
         if prev_pixel < 255:  # jezeli ostatni pixel jest "on" to doliczam tez krawedz z granica
             edge_count += 1
-    return edge_count
+            position_count += len(np_img) - index
+    return edge_count, position_count
 
 
 def yed(np_img):
     edge_count = 0
-    for col in np_img.T:
+    position_count = 0
+    for index, col in enumerate(np_img.T):
         prev_pixel = 0
         for pixel in reversed(col):
             if pixel < 255 and prev_pixel == 255:
                 edge_count += 1
+                position_count += len(np_img.T) - index
             prev_pixel = pixel
         if prev_pixel < 255:  # jezeli ostatni pixel jest "on" to doliczam tez krawedz z granica
             edge_count += 1
-    return edge_count
+            position_count += len(np_img.T) - index
+    return edge_count, position_count
 
 
 def tl2(filepath):
@@ -38,14 +44,20 @@ def tl2(filepath):
 
     img_gs = img.convert("L")
     np_img = np.array(img_gs)
+    for i, j in np.ndenumerate(np_img):
+        if np_img[i][j] > 127:
+            np_imp[i][j] = 255
+        else:
+            np_imp[i][j] = 0
+
     on_pixels = np.where(np_img < 255)
     on_pixels = np.array(on_pixels).T  # added later
 
     # tutaj uznalem ze moze lepiej bedzie najpierw policzyc koordynaty wszystkie a potem dopiero je znormalizowac
-    xbox = np.min(on_pixels[:, 1])
-    ybox = np.min(on_pixels[:, 0])
-    width = np.max(on_pixels[:, 1]) - xbox
-    height = np.max(on_pixels[:, 0]) - ybox
+    xbox = (np.max(on_pixels[:, 1]) - np.min(on_pixels[:, 1])) / 2
+    ybox = (np.max(on_pixels[:, 0]) - np.min(on_pixels[:, 0])) / 2
+    width = np.max(on_pixels[:, 1]) - np.min(on_pixels[:, 1])
+    height = np.max(on_pixels[:, 0]) - np.min(on_pixels[:, 0])
     onpix = len(on_pixels)
     xbar = (np.mean(on_pixels[:, 1]) - (xbox + 0.5 * width)) / width
     ybar = (np.mean(on_pixels[:, 0]) - (ybox + 0.5 * height)) / height
@@ -75,16 +87,16 @@ def tl2(filepath):
     xy2bar = np.mean(xy2_d)
 
     # te cztery jeszcze nie sa zaimplementowane
-    xedge = xed(np_img) / np_img.shape[0]
-    xedgey = xed(np_img)
-    yedge = yed(np_img) / np_img.shape[1]
-    yedgex = yed(np_img)
+    (xedge, xedgey) = xed(np_img)
+    (yedge, yedgex) = yed(np_img)
+    xedge = xedge / width
+    yedge = yedge / height
 
     return f'{xbox},{ybox},{width},{height},{onpix},{xbar},{ybar},{x2bar},{y2bar},{xybar},{x2ybar},{xy2bar},{xedge},{xedgey},{yedge},{yedgex}'
 
 
 directory = 'literki'
-file_csv = open('letter-recognition.csv', 'a')
+file_csv = open('letter-recognition-new.csv', 'a')
 file_csv.write('letter,xbox,ybox,width,height,onpix,xbar,ybar,x2bar,y2bar,xybar,x2ybar,xy2bar,xedge,xedgey,yedge,yedgex')
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
