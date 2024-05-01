6bcc2316d6065d5b2912c9b812a16fcdbf1de0a4
Jakub Miarka <jakvbmiarka@gmail.com>
2024-05-01 09:27:17 +0200

    added a program to predict  singular letters
    also changed a name of letterM to letterV because for some reason it was letter v, not m...

---
+++ predicter.py
@@ -0,0 +1,20 @@
+import os
+
+from csv_creation import yed, xed, tl2
+import pandas
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+
+filename = input("Podaj ścieżkę do pliku obrazu: ")
+data = tl2(filename)
+dataf = pandas.read_csv(r'letter-recognition-new.csv')
+y = dataf.pop('letter')
+x = dataf
+
+to_pred = np.array(data.split(","), ndmin=2, dtype=float)
+
+knn_lib=KNeighborsClassifier(2)
+knn_lib.fit(x, y)
+print(knn_lib.predict(to_pred))
+
