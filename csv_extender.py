2332abb851afed6525f95fdb14df4fc755c73eb9
Jakub Miarka <jakvbmiarka@gmail.com>
2024-05-21 18:51:39 +0200

    did some funny things
    actually moved image conversion functions to functions.py bc it was getting pretty buggy with the csv_extender

--- csv_extender.py
+++ csv_extender.py
@@ -1,10 +1,10 @@
-from csv_creation import tl2
+from functions import tl2
 import os
 
-directory = input("Podaj ścieżkę do folderu z literkami, które chcesz dodać do bazy danych")
+directory = input("Podaj ścieżkę do folderu z literkami, które chcesz dodać do bazy danych: ")
 file_csv = open('letter-recognition-new.csv', 'a')
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
     if os.path.isfile(f):
         data = tl2(f)
-        file_csv.write('\n' + f[len(f)-5] + ',' + data)
+        file_csv.write('\n'+f[len(f)-5] + ',' + data)
